91caf0c3294fae5c78e1db118d0096d12446184d
Jose C Gomez <jose@josecgomez.com>
2022-09-21 19:20:15 -0400

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,34 +1,48 @@
 import express from 'express';
-import bodyparser from 'body-parser'
+import bodyparser from 'body-parser';
+
 const app = express();
-
 app.use(bodyparser.json());
-app.use(bodyparser.urlencoded({ extended: true }));
+app.use(bodyparser.urlencoded({extended:true}));
+app.use('/files',express.static('public'));
 
-app.use(express.static('public'));
-app.use('/static',express.static('public'));
+let dogArray:{name:string,race:string}[]=[
+    {
+        name:"fido",
+        race:"lab"
+    },
+    {
+        name:"donkey",
+        race:"pit"
+    }
+]
 
-app.get('/Dog',(req,res,next)=>{
-    res.write('Get the Dog');
-    next();
+app.get('/Dogs',(req,res,next)=>{
+    res.send(dogArray);
+});
+app.post('/Dogs',(req,res,next)=>{
+    dogArray.push(req.body);
+    res.status(201).send(dogArray);
 });
 
-app.get('/Dog',(req,res,next)=>{
-    res.write('Get the Dog 2');
-    res.end();
+app.delete('/Dogs/:dogName',(req,res,next)=>{
+    let idx = dogArray.findIndex(d=>d.name===req.params.dogName);
+    if(idx<0)
+    {
+        res.status(404).send('Dog not Found');
+    }
+    else
+    {
+        dogArray.splice(idx,1);
+        res.status(200).send(dogArray);
+    }
+    
 });
 
-app.post('/Dog',(req,res,next)=>{
-    res.send('Create the Dog');
-});
-
-
-app.patch('/Dog/:dogName',(req,res,next)=>{
-    res.send(`Update the Dog ${req.params.dogName}`);
-});
-
-app.use('/',(req,res,next)=>{
+app.use("/",(req,res,next)=>{
+    console.log(req.url);
     res.send('Hello World');
 });
+
 
 app.listen(3000);
